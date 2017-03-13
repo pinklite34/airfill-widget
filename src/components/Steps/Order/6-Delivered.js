c6edfef26f7aec3c874ecb807f14f5ff42a82af2
Jonatan Lundin <jonatan.lundin@gmail.com>
2017-03-13 20:04:35 +0700

    Add missing Button import

--- src/components/Steps/Order/6-Delivered.js
+++ src/components/Steps/Order/6-Delivered.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import OrderStep from  '../../UI/OrderStep';
+import Button from  '../../UI/Button';
 
 const RefillDelivered = ({paymentStatus: { deliveryData = {} }, onReset}) => {
   return (
