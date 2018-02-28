28ef4a44da9492abd6f64605644ccdd19d611976
William Sandström <william@scandiblue.se>
2018-02-28 14:39:16 +0100

    listens for payment_error pusher events and payment error page

---
+++ src/components/Order/PaymentError.js
@@ -0,0 +1,51 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import Button from 'material-ui/Button';
+import OrderHeader from '../UI/OrderHeader';
+import PaymentLayout from './PaymentLayout';
+import Error from './error.svg';
+import { css } from 'glamor';
+
+const styles = {
+  textContainer: css({
+    display: 'block !important',
+    lineHeight: '21px',
+    marginRight: '48px',
+  }),
+  info: css({
+    color: '#777777',
+    fontSize: '14px',
+  }),
+  button: css({
+    marginTop: '12px',
+  }),
+};
+
+const PaymentError = props => {
+  const { order, paymentStatus: { message = '' } } = props;
+
+  return (
+    <div>
+      <OrderHeader
+        order={order}
+        title="Payment error"
+        subtitle="The payment could not be made"
+        icon={<Error />}
+      />
+      <PaymentLayout {...props}>
+        <div>
+          <div />
+          <div {...styles.textContainer}>
+            <span {...styles.info}>{message}</span>
+          </div>
+        </div>
+      </PaymentLayout>
+    </div>
+  );
+};
+
+PaymentError.propTypes = {
+  order: PropTypes.object.isRequired,
+};
+
+export default PaymentError;
