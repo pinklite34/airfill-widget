d88fb52c4a2b17fd0fbc656a420e8cb5a474a6d9
William Sandström <william@scandiblue.se>
2018-03-02 13:31:52 +0100

    temporary remove "display: none;". refs #38

--- src/lib/clipboard-helper.js
+++ src/lib/clipboard-helper.js
@@ -1,6 +1,5 @@
 export default text => {
   const element = document.createElement('textarea');
-  element.style = 'display: none;';
   element.textContent = text;
   document.body.appendChild(element);
   element.select();
