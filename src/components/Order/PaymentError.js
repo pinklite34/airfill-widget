1a5fac7069a20078e43013b08f551b01a8fce4a6
William Sandström <william@scandiblue.se>
2018-03-07 11:51:53 +0100

    set subtitle as error message

--- src/components/Order/PaymentError.js
+++ src/components/Order/PaymentError.js
@@ -3,22 +3,6 @@ import React from 'react';
 import OrderHeader from '../UI/OrderHeader';
 import PaymentLayout from './PaymentLayout';
 import Error from './error.svg';
-import { css } from 'glamor';
-
-const styles = {
-  textContainer: css({
-    display: 'block !important',
-    lineHeight: '21px',
-    marginRight: '48px',
-  }),
-  info: css({
-    color: '#777777',
-    fontSize: '14px',
-  }),
-  button: css({
-    marginTop: '12px',
-  }),
-};
 
 const PaymentError = props => {
   const { order, paymentStatus: { message = '' } } = props;
@@ -28,17 +12,10 @@ const PaymentError = props => {
       <OrderHeader
         order={order}
         title="Payment error"
-        subtitle="The payment could not be completed"
+        subtitle={message}
         icon={<Error />}
       />
-      <PaymentLayout {...props}>
-        <div>
-          <div />
-          <div {...styles.textContainer}>
-            <span {...styles.info}>{message}</span>
-          </div>
-        </div>
-      </PaymentLayout>
+      <PaymentLayout {...props} />
     </div>
   );
 };
