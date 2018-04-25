acfd57800a50ad234b4abd2aa2f99d8b4a3676f9
William Sandström <william@scandiblue.se>
2018-04-25 13:47:18 +0200

    attach select function in OperatorList

--- src/components/shared/OperatorList.js
+++ src/components/shared/OperatorList.js
@@ -1,12 +1,27 @@
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { selectAvailableOperators } from '../../store';
+import { setOperator } from '../../actions';
 
-const OperatorList = ({ children, operators }) =>
+const OperatorList = ({ children, operators, setOperator }) =>
   Object.keys(operators).map(type =>
-    operators[type].map(operator => children({ operator }))
+    operators[type].map(operator =>
+      children({ operator, select: () => setOperator(operator) })
+    )
   );
 
-export default connect(state => ({
-  operators: selectAvailableOperators(state),
-}))(OperatorList);
+OperatorList.propTypes = {
+  children: PropTypes.func.isRequired,
+  operators: PropTypes.object.isRequired,
+  setOperator: PropTypes.func.isRequired,
+};
+
+export default connect(
+  state => ({
+    operators: selectAvailableOperators(state),
+  }),
+  {
+    setOperator,
+  }
+)(OperatorList);
