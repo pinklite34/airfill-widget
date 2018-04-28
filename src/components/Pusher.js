8c4aa99464b3e02580ae653f497231659c69fe50
fred wright <fredwright0@gmail.com>
2018-04-29 01:49:40 +0700

    🔥 remove debug

--- src/components/Pusher.js
+++ src/components/Pusher.js
@@ -1,10 +1,9 @@
-import React from 'react';
 import Loadable from 'react-loadable';
 
 export default Loadable({
   loader: () =>
     import(/* webpackChunkName: "react-pusher" */ '@bitrefill/react-pusher'),
   loading: function Loader() {
-    return <h1>{'Pusher Loading'}</h1>;
+    return null;
   },
 });
