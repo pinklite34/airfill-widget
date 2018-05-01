c78fefba90b90a00bf2513880ef6229a7f0590dc
William Sandström <william@scandiblue.se>
2018-05-01 13:44:48 +0200

    field and flag components use emotion

--- src/components/UI/Flag.js
+++ src/components/UI/Flag.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css } from 'glamor';
+import { css } from 'react-emotion';
 
 import defaultFlag from '../../unknown-flag.png';
 
-const styles = {
-  image: css({
-    margin: '-3px 0',
-    width: 24,
-    height: 24,
-  }),
-};
+const style = css`
+  margin: -3px 0;
+  width: 24px;
+  height: 24px;
+`;
 
 const Flag = ({ country }) => {
   let flag;
@@ -19,7 +17,7 @@ const Flag = ({ country }) => {
     flag = require('flag-icons/flags/flags-iso/flat/24/' + country + '.png');
   } catch (ex) {}
 
-  return <img src={flag || defaultFlag} alt={country} {...styles.image} />;
+  return <img src={flag || defaultFlag} alt={country} className={style} />;
 };
 
 Flag.propTypes = {
