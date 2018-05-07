046e2ffb91a2f918b3c487dda7ac0381f67bc6c9
William Sandström <william@scandiblue.se>
2018-05-07 14:00:01 +0200

    only show receipent page and collapsed bar if relevant for current operator. refs #79

--- src/components/Receipent/index.js
+++ src/components/Receipent/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { Switch, Route } from 'react-router';
+import { Switch, Route, Redirect } from 'react-router';
+import { connect } from 'react-redux';
+
 import ReceipentCollapsed from './ReceipentCollapsed';
 import ReceipentPicker from './Receipent';
-import { configProp } from '../../lib/prop-types';
+import { configProp, operatorProp } from '../../lib/prop-types';
 
-export default function Receipent({ config }) {
+import { isValidEmail } from '../../lib/email-validation';
+import { selectOperator } from '../../store';
+
+function Receipent({ config, operator }) {
+  const showEmail = !isValidEmail(config.orderOptions.email);
+  const showNumber = !operator.result || !operator.result.noNumber;
+
+  const show = showEmail || showNumber;
   return (
     <Switch>
       <Route path="/refill" exact />
       <Route path="/refill/selectProvider" />
       <Route path="/refill/selectAmount" />
-      <Route
-        path="/refill/selectReceipent"
-        render={props => <ReceipentPicker {...props} config={config} />}
-      />
-      <Route component={ReceipentCollapsed} />
+      {show && (
+        <Route
+          path="/refill/selectReceipent"
+          render={props => <ReceipentPicker {...props} config={config} />}
+        />
+      )}
+      {show && <Route component={ReceipentCollapsed} />}
+      {!show && <Redirect to="/refill/selectPayment" />}
     </Switch>
   );
 }
 
 Receipent.propTypes = {
   config: configProp,
+  operator: operatorProp,
 };
+
+export default connect(state => ({ operator: selectOperator(state) }))(
+  Receipent
+);
