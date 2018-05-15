76c9935a8c934a92dd3db0bb6a5c8433731e4a16
William Sandström <william@scandiblue.se>
2018-05-15 10:28:32 +0200

    dispatch

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ export const initializeWidget = apiKey => (dispatch, getState) => {
     baseUrl: '/api/widget',
   });
 
-  init({})(dispatch, getState);
+  dispatch(init({}));
 };
 
 export { airfillWidget, widgetStoreEnhancer, client as restClient };
