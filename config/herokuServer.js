25c19a728a1cc1f621b47b76e1da9203c604634e
Maxim Orlov <maximorl@gmail.com>
2018-06-09 19:29:43 +0800

    Fix express static file server path

--- config/herokuServer.js
+++ config/herokuServer.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
