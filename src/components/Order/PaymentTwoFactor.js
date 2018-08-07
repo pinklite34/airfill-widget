0ee9c37ea11fa4be76610e364f7de6b37a27ffc0
William Sandström <william@scandiblue.se>
2018-08-07 12:47:28 +0200

    add onclick function, store 2fa code in state

--- src/components/Order/PaymentTwoFactor.js
+++ src/components/Order/PaymentTwoFactor.js
@@ -9,46 +9,51 @@ import Button from '../UI/Button';
 import Info from '../UI/info.svg';
 import Text from '../UI/Text';
 
-export default function PaymentTwoFactor(props) {
-  const { order } = props;
+export default class PaymentTwoFactor extends React.Component {
+  state = {
+    code: '',
+  };
 
-  // TODO
-  // onClick = () => {
-  //
-  // };
+  onClick = () => {};
 
-  return (
-    <Fragment>
-      <OrderHeader
-        order={order}
-        title={{ id: 'order.2farequired.title', children: '2FA Required' }}
-        subtitle={{
-          id: 'order.2farequired.subtitle',
-          children: 'We need your Coinbase 2FA code to complete the payment',
-        }}
-        icon={<Info tight />}
-      />
+  render() {
+    const { order } = this.props;
+    const { code } = this.state;
 
-      <PaymentLayout {...props}>
-        <Text>Enter 2-step verification code:</Text>
-        <Input
-          type="text"
-          id="2fa"
-          ref={c => (this.twoFactorCode = c)}
-          required
-        />
-
-        <Button
-          margin="12px 0 0"
-          onClick={this.onClick}
-          text={{
-            id: 'paymentmethod.coinbase.pay',
-            children: 'Pay with Coinbase',
+    return (
+      <Fragment>
+        <OrderHeader
+          order={order}
+          title={{ id: 'order.2farequired.title', children: '2FA Required' }}
+          subtitle={{
+            id: 'order.2farequired.subtitle',
+            children: 'We need your Coinbase 2FA code to complete the payment',
           }}
+          icon={<Info tight />}
         />
-      </PaymentLayout>
-    </Fragment>
-  );
+
+        <PaymentLayout {...this.props}>
+          <Text>Enter 2-step verification code:</Text>
+          <Input
+            type="text"
+            id="2fa"
+            required
+            onChange={e => this.setState({ code: e.target.value.trim() })}
+            value={code}
+          />
+
+          <Button
+            margin="12px 0 0"
+            onClick={this.onClick}
+            text={{
+              id: 'paymentmethod.coinbase.pay',
+              children: 'Pay with Coinbase',
+            }}
+          />
+        </PaymentLayout>
+      </Fragment>
+    );
+  }
 }
 
 PaymentTwoFactor.propTypes = {
