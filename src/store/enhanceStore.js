362e7d3ace2863b4da9bb2a97a6ee700b2c1f087
William Sandström <william@scandiblue.se>
2018-08-07 13:50:01 +0200

    remove selected operator from persisted store

--- src/store/enhanceStore.js
+++ src/store/enhanceStore.js
@@ -1,7 +1,11 @@
 import { persistStore } from 'redux-persist';
 
 export default function enhanceStore(store) {
-  persistStore(store, { whitelist: 'airfillWidget', keyPrefix: 'airfill' });
+  persistStore(store, {
+    whitelist: 'airfillWidget',
+    blacklist: 'airfillWidget.operator',
+    keyPrefix: 'airfill',
+  });
 
   if (window && window.localStorage && window.localStorage.removeItem) {
     window.localStorage.removeItem('reduxPersist:airfillWidget');
