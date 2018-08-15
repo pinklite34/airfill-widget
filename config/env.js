27ad148e0c3dd755ca9b67d2bea87e37f7146006
fred wright <fredwright0@gmail.com>
2018-08-15 19:02:34 +0100

    🐞 fix pusher env key

--- config/env.js
+++ config/env.js
@@ -1,6 +1,14 @@
+'use strict';
+
 const fs = require('fs');
+const path = require('path');
 
 const NODE_ENV = process.env.NODE_ENV;
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.'
+  );
+}
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
 var dotenvFiles = [
@@ -28,8 +36,30 @@ dotenvFiles.forEach(dotenvFile => {
   }
 });
 
+// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// injected into the application via DefinePlugin in Webpack configuration.
+const REACT_APP = /^REACT_APP_/i;
+
 function getClientEnvironment(publicUrl, target) {
-  const raw = Object.keys(process.env);
+  const raw = Object.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key.replace(REACT_APP, '')] = process.env[key];
+        return env;
+      },
+      {
+        // Useful for determining whether we’re running in production mode.
+        // Most importantly, it switches React into the correct mode.
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        // Useful for resolving the correct path to static assets in `public`.
+        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+        // This should only be used as an escape hatch. Normally you would put
+        // images into the `src` and `import` them in code to get their paths.
+        LNG: process.env.LNG,
+        LOCIZE_PROJECT_ID: process.env.LOCIZE_PROJECT_ID,
+      }
+    );
 
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
